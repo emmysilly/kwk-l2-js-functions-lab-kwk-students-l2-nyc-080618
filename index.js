57c5ef6559046edbd1e491d6c26f5e49c88fb86d
emmysilly <emelybchavez@gmail.com>
2018-08-08 19:39:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+var result = 0
+function distanceTravelledInFeet(start, destination){
+  if (destination < start){
+     result = (start - destination) * 264;
+  } else { result = (destination - start) * 264}
+  return result;
+}
+ distanceTravelledInFeet(34, 38);
+ 
+ 
+var blocks = 0
+function distanceFromHqInBlocks (someValue) {
+}
+  if (42 < someValue){
+     blocks = (someValue - 42);
+  } else { blocks = (42 - someValue)}
+  return blocks;
+}
+ distanceTravelledInFeet(34, 38);
